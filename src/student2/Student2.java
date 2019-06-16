229a804f686724f308a75ae5fb628d2ac331ab67
alsaddir <alsaddir@sheridancollege.ca>
2019-06-16 13:22:00 -0400

    new repo to the project

---
+++ src/student2/Student2.java
@@ -0,0 +1,41 @@
+package student2;
+import java.util.Scanner;
+/**
+ *
+ * @author Rich Smith at ZenOfProgramming.com
+ */
+public class Student2
+{
+   int id;
+   String name;
+
+   /**
+    * @param args the command line arguments
+    */
+   public static void main (String[] args)
+   {
+
+
+
+      Student2 s = new Student2();
+      Student2[] list = new Student2[5];
+//        list[0].id=10;
+//        list[0].name="Megha";
+      Scanner in = new Scanner(System.in);
+
+      for (int i = 0; i <= list.length - 1; i++) {
+         list[i] = new Student2();
+         System.out.print("Enter student id: ");
+         list[i].id = in.nextInt();
+         System.out.print("Enter student name: ");
+         list[i].name = in.next();
+
+      }
+
+      for (int i = 0; i <= list.length - 1; i++) {
+         System.out.print("student id: " + list[i].id + "student name:" + list[i].name + "\n");
+
+      }
+
+   }
+}
